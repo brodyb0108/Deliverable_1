0ba06ce82d0e0cb706763664ddf4675629844b2a
Brody <Brody@DESKTOP-S685LHF>
2023-07-03 15:32:39 -0400

    Add project files.

---
+++ Deliverable_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+Console.WriteLine("Enter an integer number bewtween 1 and 100:");
+int input = int.Parse(Console.ReadLine());
+
+Console.Write("Specify the series bye: Even or Odd");
+string series = Console.ReadLine();
+
+if (series == "Odd")
+{
+	Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + "are:");
+	for (int i = 0; i < input; i = i + 2)
+	{
+		Console.WriteLine(i);
+	}
+}
+
+if (series == "Even")
+{
+	Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + "are:");
+	for (int i = 0; i < input; i = i + 2)
+	{
+		Console.WriteLine(i);
+	}
+}
